5b2c697931d6dfb3766a8a09483d35e0dd56789f
roshn <roshn@131.229.43.179>
2019-09-13 16:29:04 -0400

    fish class

--- src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -7,15 +7,22 @@ public class Fish {
 	Color color;
 	int x;
 	int y;
+	boolean isLittle;
 	
-	public Fish(Color c, int startX, int startY) {
+	public Fish(Color c, int startX, int startY, boolean isLittle) {
 		this.color = c;
 		this.x = startX;
 		this.y = startY;
+		this.isLittle = isLittle;
 		}
 	public void draw(Graphics2D g) {
+		if(this.isLittle) {
 		DrawFish.smallFacingLeft(g, this.color, this.x, this.y);
 		this.swim();
+		} else {
+		DrawFish.faingLeft(g, this.color, this.x, this.y);
+		this.swim();	
+		}
 	}
 	public void swim() {
 		this.x += 1;
